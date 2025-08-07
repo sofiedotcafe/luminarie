afa95ed847b856d1b9132a8bfaa9c9354b9617e7
sofiedotcafe <sofie.halenius@sofie.cafe>
2025-08-07 22:01:52 +0300

    feat(nixos/modules): openrgb

---
+++ modules/nixos/services/openrgb/default.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+let
+  cfg = config.modules.nixos.services.openrgb;
+in
+{
+  options.modules.nixos.services.openrgb = {
+    enable = mkEnableOption "openrgb";
+
+    package = mkOption {
+      type = types.package;
+      default = pkgs.openrgb-with-all-plugins.overrideAttrs (_: {
+        version = "0-unstable-2025-08-06";
+        src = pkgs.fetchFromGitLab {
+          owner = "CalcProgrammer1";
+          repo = "OpenRGB";
+          rev = "e6190ec2756fe99971f057621efc234680db79ec";
+          hash = "sha256-Q64ouAIJFxp5cmt+zjYPqIHjcXBz3Ueulj7/lAsOVVA=";
+        };
+      });
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.hardware.openrgb = {
+      enable = true;
+      inherit package;
+    };
+    environment.systemPackages = [ cfg.package ];
+  };
+}
